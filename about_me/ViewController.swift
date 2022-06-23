a84e659d40028ee37b270324c3c828b9617facd2
Grace Nelson <graceenelson7@gmail.com>
2022-06-23 11:46:45 -0500

    iteration one

--- about_me/ViewController.swift
+++ about_me/ViewController.swift
@@ -15,5 +15,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var image: UIImageView!
+    @IBAction func buttonOne(_ sender: Any) {
+        image.image = UIImage(named: "family")
+        button1.backgroundColor = UIColor.white
+    }
+    @IBOutlet weak var button1: UIButton!
+    @IBAction func buttonThree(_ sender: Any) {
+        image.image = UIImage(named: "pets")
+        button3.backgroundColor = UIColor.white
+    }
+    @IBOutlet weak var button3: UIButton!
+    @IBAction func buttonFour(_ sender: Any){
+        image.image = UIImage(named: "picture1")
+        button4.backgroundColor = UIColor.white
+    }
+    @IBOutlet weak var button4: UIButton!
+    @IBAction func buttonTwo(_ sender: Any) {
+        image.image = UIImage(named: "hobbies")
+        button2.backgroundColor = UIColor.white
+    }
+    @IBOutlet weak var button2: UIButton!
 }
-
